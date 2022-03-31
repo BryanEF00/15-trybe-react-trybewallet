2afaa6fc1c304c61a1c0a2b8125d3efe12eed75a
Bryan Edward Fernandes <bryan.efernandes@gmail.com>
2022-03-31 17:01:57 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,88 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
+import { saveEmail } from '../redux/actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      isDisabled: true,
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({ [name]: value }, this.loginValidation);
+  }
+
+  loginValidation = () => {
+    const { email, password } = this.state;
+
+    const regexTest = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const maxLength = 6;
+
+    const validEmail = regexTest.test(email);
+    const validPassword = password.length >= maxLength;
+
+    const isInvalid = !(validEmail && validPassword);
+
+    this.setState({ isDisabled: isInvalid });
+  }
+
+  handleLogin = (event) => {
+    event.preventDefault();
+
+    const { email } = this.state;
+    const { history, userEmail } = this.props;
+
+    userEmail(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, isDisabled } = this.state;
+
+    return (
+      <div>
+        <form>
+          <input
+            data-testid="email-input"
+            type="email"
+            name="email"
+            value={ email }
+            onChange={ this.onInputChange }
+          />
+          <input
+            data-testid="password-input"
+            type="password"
+            name="password"
+            value={ password }
+            onChange={ this.onInputChange }
+          />
+          <button
+            type="submit"
+            disabled={ isDisabled }
+            onClick={ this.handleLogin }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>);
   }
 }
 
-export default Login;
+Login.propTypes = {
+  userEmail: PropTypes.func.isRequired,
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  userEmail: (email) => dispatch(saveEmail(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
---
+++ src/redux/actions/index.js
@@ -0,0 +1,5 @@
+export const SAVE_EMAIL = 'SAVE_EMAIL';
+
+export const GET_EMAIL = 'GET_EMAIL';
+
+export const saveEmail = (email) => ({ type: SAVE_EMAIL, email });
