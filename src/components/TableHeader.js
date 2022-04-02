efcebeee351cfec2755278aa2b05b5b359b52256
Bryan Edward Fernandes <bryan.efernandes@gmail.com>
2022-04-02 12:48:51 -0300

    Requisito 8

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -1,25 +1,77 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class TableHeader extends Component {
+  getExchangeInfo = (exchangeRates, currency) => {
+    const entries = Object.entries(exchangeRates);
+    const getRate = entries.find(([key]) => key === currency)[1];
+
+    const { name, ask } = getRate;
+    const formattedName = name.split('/')[0];
+
+    const currencyName = formattedName === 'Dólar Americano'
+      ? `${formattedName.replace('Americano', 'Comercial')} `
+      : formattedName;
+
+    const data = { currencyName, ask };
+
+    return data;
+  }
+
   render() {
+    const { expensesInfo } = this.props;
+
+    const tableData = [
+      'Descrição', 'Tag', 'Método de pagamento', 'Valor', 'Moeda',
+      'Câmbio utilizado', 'Valor convertido', 'Moeda de conversão',
+      'Editar/Excluir',
+    ];
+
     return (
       <table>
         <thead>
           <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
+            {tableData.map((field, index) => (<th key={ index }>{field}</th>))}
           </tr>
         </thead>
+        <tfoot>
+          {expensesInfo.map((expense) => {
+            const {
+              id, value, description, currency, method, tag, exchangeRates,
+            } = expense;
+
+            const { currencyName, ask } = this.getExchangeInfo(exchangeRates, currency);
+
+            const fixedValue = parseFloat(value).toFixed(2);
+            const fixedAsk = parseFloat(ask).toFixed(2);
+            const conversionCurrency = 'Real';
+
+            const convertedValue = (fixedValue * ask).toFixed(2);
+
+            const rowInfo = [
+              description, tag, method, fixedValue, currencyName, fixedAsk,
+              convertedValue, conversionCurrency,
+            ];
+
+            return (
+              <tr key={ id }>
+                {rowInfo.map((info) => (<td key={ `id:${info}` }>{info}</td>))}
+              </tr>
+            );
+          })}
+        </tfoot>
       </table>
     );
   }
 }
 
-export default TableHeader;
+TableHeader.propTypes = {
+  expensesInfo: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+const mapStateToProps = (store) => ({
+  expensesInfo: store.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(TableHeader);
